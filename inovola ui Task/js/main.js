4d872303accae38c0f39de1728111f9c2d73b574
Basem-Serag <basemserag.911@gmail.com>
2020-07-26 03:07:46 +0200

    bn

---
+++ inovola ui Task/js/main.js
@@ -0,0 +1,38 @@
+window.onscroll = () => {
+        $(window).on('scroll', function () {
+            if ($(window).scrollTop()) {
+                $('nav').addClass('black');
+            } else {
+                $('nav').removeClass('black');
+            }
+        });
+    }
+        $('.owl-carousel').owlCarousel({
+            loop: true,
+            margin: 10,
+            nav: false,
+            autoplay: true,
+            autoplayTimeout: 5000,
+            autoplayHoverPause: true,
+            responsive: {
+                0: {
+                    items: 1
+                },
+                600: {
+                    items: 1
+                },
+                1000: {
+                    items: 1
+                }
+            }
+        });
+
+        $('input').on('focusin', function () {
+            $(this).parent().find('label').addClass('active');
+        });
+
+        $('input').on('focusout', function () {
+            if (!this.value) {
+                $(this).parent().find('label').removeClass('active');
+            }
+        });
