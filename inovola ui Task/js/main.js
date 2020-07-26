2c80807640eddcdc40a464a04c685b264be029a4
Basem-Serag <basemserag.911@gmail.com>
2020-07-27 01:20:56 +0200

    quizz

--- inovola ui Task/js/main.js
+++ inovola ui Task/js/main.js
@@ -1,38 +1,40 @@
 window.onscroll = () => {
-        $(window).on('scroll', function () {
-            if ($(window).scrollTop()) {
-                $('nav').addClass('black');
-            } else {
-                $('nav').removeClass('black');
-            }
-        });
+    $(window).on('scroll', function () {
+        if ($(window).scrollTop()) {
+            $('nav').addClass('bg');
+        } else {
+            $('nav').removeClass('bg');
+        }
+    });
+}
+$('.owl-carousel').owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: false,
+    autoplay: true,
+    autoplayTimeout: 5000,
+    autoplayHoverPause: true,
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 1
+        },
+        1000: {
+            items: 1
+        }
     }
-        $('.owl-carousel').owlCarousel({
-            loop: true,
-            margin: 10,
-            nav: false,
-            autoplay: true,
-            autoplayTimeout: 5000,
-            autoplayHoverPause: true,
-            responsive: {
-                0: {
-                    items: 1
-                },
-                600: {
-                    items: 1
-                },
-                1000: {
-                    items: 1
-                }
-            }
-        });
+});
 
-        $('input').on('focusin', function () {
-            $(this).parent().find('label').addClass('active');
-        });
+$('input').on('focusin', function () {
+    $(this).parent().find('label').addClass('active');
+});
 
-        $('input').on('focusout', function () {
-            if (!this.value) {
-                $(this).parent().find('label').removeClass('active');
-            }
-        });
+$('input').on('focusout', function () {
+    if (!this.value) {
+        $(this).parent().find('label').removeClass('active');
+    }
+});
+
+AOS.init();
